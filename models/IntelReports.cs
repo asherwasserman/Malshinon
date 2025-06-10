c1f3af455b5c85c9e8c938aa03c20aac64717948
asher <aw0534145493@gmail.com>
2025-06-10 10:12:08 +0300

    Initializing the project with basic classes

---
+++ models/IntelReports.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon.models
+{
+    public class IntelReports
+    {
+        public int id { get; set; }
+        public int reporterId { get; set; }
+        public int targetId { get; set; }
+        public string intelText { get; set; }
+        public DateTime timeStemp {get; set; }
+        
+    }
+}
