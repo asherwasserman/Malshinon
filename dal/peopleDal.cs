84510c32d2181799070ea5cdbf1a2923c49a3860
asher <aw0534145493@gmail.com>
2025-06-10 09:59:13 +0300

    adding a create new people method

---
+++ dal/peopleDal.cs
@@ -0,0 +1,38 @@
+﻿using MySqlX.XDevAPI.CRUD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Malshinon.models;
+using MySql.Data.MySqlClient;
+
+namespace Malshinon.dal
+{
+    public class peopleDal
+    {
+        public  MySQLData mySqlData = new MySQLData();
+        public void Create(People person)
+        {
+            string firstName = person.firstName;
+            string lastName = person.lastName;
+            string secertCode = person.secertCODE;
+            string type = person.type;
+            int numReports = person.numReports;
+            int numMentions = person.numMentions;
+            string query = $"INSERT INTO people(first_name, last_name, secert_code, type, num_reports, num_mentions) " +
+               $"VALUES('{firstName}', '{lastName}',  '{secertCode}', '{type}', '{numReports}', '{numMentions}');";
+            MySqlCommand cmd = new MySqlCommand(query);
+            try
+            {
+                mySqlData.GetConnection();
+                cmd.ExecuteNonQuery();
+                mySqlData.CloseConnection();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Query error: {ex}");
+            }
+        }
+    }
+}
