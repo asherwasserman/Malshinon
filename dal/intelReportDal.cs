92fbc531a6657ac00eebc67d0569b33eeef85c69
asher <aw0534145493@gmail.com>
2025-06-11 12:00:43 +0300

    adding people controler class

--- dal/intelReportDal.cs
+++ dal/intelReportDal.cs
@@ -1,4 +1,5 @@
-﻿using Mysqlx.Crud;
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,30 @@ namespace Malshinon.models
 {
     public class IntelReportDal
     {
+        MySQLData mySQL = new MySQLData();
+
         public void create(IntelReports intelReport)
-        {
-            int id = intelReport.id;
-            int reportId = intelReport.reporterId;
-            int targetId = intelReport.targetId;
-            string intelText = intelReport.intelText;
-            DateTime timeStemp = intelReport.timeStemp;
+        {           
+            try
+            {
+                int reportId = intelReport.reporterId;
+                int targetId = intelReport.targetId;
+                string intelText = intelReport.intelText;
+                DateTime timeStemp = intelReport.timeStemp;
+                string query = $"INSERT INTO intel_reports ( report_id, target_id, intel_text, time_stemp)" +
+                    $" VALUES ('{reportId}','{targetId}','{intelText}','{timeStemp}')";
+                mySQL.GetConnection();
+                MySqlCommand cmd = new MySqlCommand(query);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Query error: {ex.Message}");
+            }
+            finally
+            {
+                mySQL.CloseConnection();
+            }
 
         }
     }
